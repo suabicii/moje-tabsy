c4390d3bfa89a6754107c51ed58d139755dc3311
suabicii <michal.slabik3@wp.pl>
2022-06-08 17:39:41 +0200

    Updated User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,44 +4,44 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private ?int $id;
 
+    #[Assert\NotBlank(message: "Please enter a name")]
     #[ORM\Column(type: 'string', length: 255)]
-    private ?string $name;
+    private $name;
 
-    #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank(message: "Please enter a valid email address.")]
     #[Assert\Email]
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
     private ?string $email;
 
-    /** @var string The hashed password  */
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
+
     #[Assert\NotBlank(message: "Please enter a valid password.")]
+    #[ORM\Column(type: 'string')]
     private string $password;
+
+    #[Assert\GreaterThan(0)]
+    #[ORM\Column(type: 'string', length: 3, nullable: true)]
+    private ?string $tel_prefix;
+
+    #[ORM\Column(type: 'string', length: 9, nullable: true)]
+    private ?string $tel;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -56,7 +56,39 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -64,6 +96,51 @@ class User
     public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getTelPrefix(): ?string
+    {
+        return $this->tel_prefix;
+    }
+
+    public function setTelPrefix(?string $tel_prefix): self
+    {
+        $this->tel_prefix = $tel_prefix;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(?string $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
 
         return $this;
     }
