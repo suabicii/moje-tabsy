1c6fccba2f5df41b674af0567f738a53de0785e2
suabicii <michal.slabik3@wp.pl>
2022-07-01 18:23:00 +0200

    Removed unnecessary AppRoute's child in root's render method

--- assets/ReactApp/react-app.js
+++ assets/ReactApp/react-app.js
@@ -1,11 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import AppRouter from "./routers/AppRouter";
-import Header from "./components/Header";
 
 const root = createRoot(document.getElementById('react'));
-root.render(
-    <AppRouter>
-        <Header/>
-    </AppRouter>
-);
+root.render(<AppRouter/>);
