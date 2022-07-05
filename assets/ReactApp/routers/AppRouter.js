783eda15fa13c1066a91871b3d68f7cd096de48f
suabicii <michal.slabik3@wp.pl>
2022-07-05 13:15:30 +0200

    Added not found React route

--- assets/ReactApp/routers/AppRouter.js
+++ assets/ReactApp/routers/AppRouter.js
@@ -5,6 +5,7 @@ import Summary from "../components/Summary";
 import DrugList from "../components/DrugList";
 import Profile from "../components/Profile";
 import Settings from "../components/Settings";
+import NotFound from "../components/NotFound";
 
 export const mainRoute = "/dashboard";
 
@@ -16,6 +17,7 @@ function AppRouter() {
                 <Route path={`${mainRoute}/drug-list`} element={<DrugList/>} />
                 <Route path={`${mainRoute}/profile`} element={<Profile/>} />
                 <Route path={`${mainRoute}/settings`} element={<Settings/>} />
+                <Route path={`${mainRoute}/*`} element={<NotFound/>} />
             </Routes>
             <Header/>
         </BrowserRouter>
