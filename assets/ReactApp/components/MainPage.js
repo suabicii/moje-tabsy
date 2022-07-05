196578b92fabcf5bb8834c6b37d422076cee7a95
suabicii <michal.slabik3@wp.pl>
2022-07-05 18:52:31 +0200

    Removed unnecessary comments in MainPage

--- assets/ReactApp/components/MainPage.js
+++ assets/ReactApp/components/MainPage.js
@@ -3,13 +3,11 @@ import AppRouter from "../routers/AppRouter";
 
 function MainPage() {
     return (
-        //Main layout-->
         <main className="dashboard__main">
             <div className="container py-4">
                 <AppRouter/>
             </div>
         </main>
-        //Main layout-->
     );
 }
 
