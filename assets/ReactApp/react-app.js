c630c76cc7e423b1f3448a6866b30410fcae4be1
suabicii <michal.slabik3@wp.pl>
2022-07-26 12:46:12 +0200

    Added getting drug list from local storage (temporarily)

--- assets/ReactApp/react-app.js
+++ assets/ReactApp/react-app.js
@@ -3,42 +3,7 @@ import {createRoot} from "react-dom/client";
 import MainPage from "./components/MainPage";
 import {DrugListContainer} from "./container/DrugListContainer";
 
-const drugList = [
-    {
-        id: 1,
-        name: 'Xanax',
-        quantity: 60,
-        quantityMax: 120,
-        unit: 'szt.',
-        dosing: 1,
-        dosingMoments: {
-            1: '07:00',
-            2: '18:00'
-        }
-    },
-    {
-        id: 2,
-        name: 'Witamina C',
-        quantity: 20,
-        quantityMax: 80,
-        unit: 'szt.',
-        dosing: 1,
-        dosingMoments: {
-            1: '12:00'
-        }
-    },
-    {
-        id: 3,
-        name: 'Metanabol',
-        quantity: 4,
-        quantityMax: 10,
-        unit: 'amp. 10 ml',
-        dosing: 1,
-        dosingMoments: {
-            1: '17:00'
-        }
-    },
-];
+const drugList = JSON.parse(localStorage.getItem('drugs')) || [];
 
 const root = createRoot(document.getElementById('react'));
 root.render(
