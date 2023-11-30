e76060544287d04ef79100d9e8d66092c32bb5d5
Michael Slabikovsky <michal.slabik.programista@gmail.com>
2023-11-30 19:13:04 +0100

    Added place for out of stock dates

--- assets/ReactApp/components/Summary.js
+++ assets/ReactApp/components/Summary.js
@@ -15,7 +15,7 @@ function Summary() {
             <h1 className="text-center mt-5 mt-md-0">Podsumowanie</h1>
             <DrugList isEditMode={false} isEmpty={!drugList.length}/>
             <div className="row mt-3">
-                <div className="col-md-6">
+                <div className="col-md-4">
                     <div className="card">
                         <div className="card-header text-center">
                             Dzisiaj muszę zażyć <i className="fa-solid fa-clock"></i>
@@ -26,7 +26,7 @@ function Summary() {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-6 mt-3 mt-md-0">
+                <div className="col-md-4 mt-3 mt-md-0">
                     <div className="card">
                         <div className="card-header text-center">
                             Stan zapasów <i className="fa-solid fa-warehouse"></i>
@@ -34,6 +34,16 @@ function Summary() {
                         <div
                             className={`card-body ${drugList.length < 1 ? 'd-flex flex-column justify-content-center' : ''}`}>
                             {drugList.length > 0 ? <StockStatusChecker/> : <EmptyDrugListInfo/>}
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4 mt-3 mt-md-0">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            Kiedy zapasy zostaną wyczerpane? <i className="fa-solid fa-calendar-days"></i>
+                        </div>
+                        <div className="card-body d-flex flex-column justify-content-center">
+                            <p className="text-center">Miejsce na daty</p>
                         </div>
                     </div>
                 </div>
