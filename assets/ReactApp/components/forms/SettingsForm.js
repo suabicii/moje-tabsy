d6f77da2c51376241b43e57d0b6cc6b7cc3e43df
Michael Slabikovsky <michal.slabik.programista@gmail.com>
2024-01-19 17:42:18 +0100

    Added dark mode switcher

--- assets/ReactApp/components/forms/SettingsForm.js
+++ assets/ReactApp/components/forms/SettingsForm.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 function SettingsForm() {
+    const darkModeDefault = JSON.parse(localStorage.getItem('darkMode')) || false;
+    const [darkMode, setDarkMode] = useState(darkModeDefault);
+
     return (
         <div className="card mt-3">
             <div className="card-header text-center">Ustawienia ogólne</div>
             <div className="card-body">
                 <form role="form" name="settings_form" onSubmit={e => {
                     e.preventDefault();
+                    localStorage.setItem('darkMode', `${darkMode}`);
                 }}>
                     <div className="notifier-choice">
                         <label className="form-label">Ciemny motyw:</label>
                         <div className="form-check form-switch">
-                            <input type="checkbox" name="messengerCheck" id="messengerCheck"
-                                   className="form-check-input" role="switch"/>
-                            <label htmlFor="messengerCheck" className="form-check-label">Wyłączony</label>
+                            <input type="checkbox" name="messengerCheck" id="dark-mode-switcher"
+                                   className="form-check-input" role="switch" data-testid="dark-mode-switcher"
+                                   checked={darkMode}
+                                   onChange={() => setDarkMode(prevState => !prevState)}/>
+                            <label htmlFor="dark-mode-switcher"
+                                   className="form-check-label">{darkMode ? 'Włączony' : 'Wyłączony'}</label>
                         </div>
                     </div>
                     <div className="d-grid mt-3">
-                        <button className="btn btn-primary">Zapisz <i className="fa-solid fa-floppy-disk"></i></button>
+                        <button className="btn btn-primary" data-testid="settings-submit">
+                            Zapisz <i className="fa-solid fa-floppy-disk"></i>
+                        </button>
                     </div>
                 </form>
             </div>
--- assets/ReactApp/tests/components/forms/SettingsForm.test.js
+++ assets/ReactApp/tests/components/forms/SettingsForm.test.js
@@ -4,9 +4,29 @@
 import React from "react";
 import ReactShallowRenderer from "react-test-renderer/shallow";
 import SettingsForm from "../../../components/forms/SettingsForm";
+import {fireEvent, render, screen} from "@testing-library/react";
 
 it('should correctly render SettingsForm', () => {
     const renderer = new ReactShallowRenderer();
     renderer.render(<SettingsForm/>);
     expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+it('should save dark mode state to localStorage after form submission', () => {
+    render(<SettingsForm/>);
+
+    fireEvent.click(screen.getByTestId('settings-submit'));
+    const darkModeState = localStorage.getItem('darkMode');
+
+    expect(darkModeState).toBeTruthy();
+});
+
+it('should save dark mode state to localStorage after checkbox change and form submission', () => {
+    render(<SettingsForm/>);
+
+    fireEvent.click(screen.getByTestId('dark-mode-switcher'));
+    fireEvent.click(screen.getByTestId('settings-submit'));
+    const darkModeState = localStorage.getItem('darkMode');
+
+    expect(darkModeState).toBeTruthy();
 });
