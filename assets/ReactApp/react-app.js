abb88b5902b8ab9c1682719ef50aeeedc0dc2475
Michael Slabikovsky <michal.slabik.programista@gmail.com>
2024-01-24 11:17:55 +0100

    Implemented changing theme functionality

--- assets/ReactApp/react-app.js
+++ assets/ReactApp/react-app.js
@@ -3,8 +3,12 @@ import {createRoot} from "react-dom/client";
 import MainPage from "./components/MainPage";
 import {Provider} from "react-redux";
 import store from "./store";
+import {setDarkMode} from "./features/darkMode/darkModeSlice";
 
 const root = createRoot(document.getElementById('react'));
+
+const darkModeFromLocalStorage = !!JSON.parse(localStorage.getItem('darkMode'));
+store.dispatch(setDarkMode(darkModeFromLocalStorage));
 
 root.render(
     <Provider store={store}>
--- assets/ReactApp/tests/components/forms/SettingsForm.test.js
+++ assets/ReactApp/tests/components/forms/SettingsForm.test.js
@@ -5,15 +5,41 @@ import React from "react";
 import ReactShallowRenderer from "react-test-renderer/shallow";
 import SettingsForm from "../../../components/forms/SettingsForm";
 import {fireEvent, render, screen} from "@testing-library/react";
+import {Provider} from "react-redux";
+import store from "../../../store";
+import changeTheme from "../../../utils/changeTheme";
+
+jest.mock('../../../utils/changeTheme', () => ({
+    __esModule: true,
+    default: jest.fn()
+}));
+
+beforeAll(() => {
+    changeTheme.mockImplementation(darkMode => {
+        console.log(`Dark mode in SettingsForm was set to: ${darkMode}`);
+    });
+});
+
+afterAll(() => {
+    jest.clearAllMocks();
+});
+
+function WrappedComponent() {
+    return (
+        <Provider store={store}>
+            <SettingsForm/>
+        </Provider>
+    );
+}
 
 it('should correctly render SettingsForm', () => {
     const renderer = new ReactShallowRenderer();
-    renderer.render(<SettingsForm/>);
+    renderer.render(<WrappedComponent/>);
     expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
 
 it('should save dark mode state to localStorage after form submission', () => {
-    render(<SettingsForm/>);
+    render(<WrappedComponent/>);
 
     fireEvent.click(screen.getByTestId('settings-submit'));
     const darkModeState = localStorage.getItem('darkMode');
@@ -22,7 +48,7 @@ it('should save dark mode state to localStorage after form submission', () => {
 });
 
 it('should save dark mode state to localStorage after checkbox change and form submission', () => {
-    render(<SettingsForm/>);
+    render(<WrappedComponent/>);
 
     fireEvent.click(screen.getByTestId('dark-mode-switcher'));
     fireEvent.click(screen.getByTestId('settings-submit'));
