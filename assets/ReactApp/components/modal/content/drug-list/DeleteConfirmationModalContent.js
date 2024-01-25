d24c61235a9dd461d671be79e39d8750695ec6ea
Michael Slabikovsky <michal.slabik.programista@gmail.com>
2024-01-25 20:24:15 +0100

    Fixed delete confirmation modal styles

--- assets/ReactApp/components/modal/content/drug-list/DeleteConfirmationModalContent.js
+++ assets/ReactApp/components/modal/content/drug-list/DeleteConfirmationModalContent.js
@@ -1,15 +1,18 @@
 import React from "react";
+import {useSelector} from "react-redux";
 
-function DeleteConfirmationModalContent({setIsModalOpen}) {
+function DeleteConfirmationModalContent({setIsModalOpen, confirmDeletion, drugId}) {
+    const darkMode = useSelector(state => state.darkMode);
+
     return (
         <div className="container text-center" role="dialog" data-testid="delete-confirmation">
-            <h2 className="text-success">Czy na pewno chcesz usunąć dany lek/suplement?</h2>
-            <div className="grid mt-2">
-                <button className="btn btn-danger me-lg-4" data-testid="yes" onClick={() => {
+            <h2 className="text-danger-emphasis">Czy na pewno chcesz usunąć dany lek/suplement?</h2>
+            <div className="grid mt-4">
+                <button className="btn btn-danger me-lg-2" data-testid="yes" onClick={() => {
                     setIsModalOpen(false);
                 }}>Tak <i className="fa-solid fa-check"></i>
                 </button>
-                <button className="btn btn-outline-dark ms-lg-2 mt-lg-0 mt-2" data-testid="no" onClick={() => {
+                <button className={`btn btn-${darkMode ? 'light' : 'dark'} ms-lg-2 mt-lg-0 mt-2`} data-testid="no" onClick={() => {
                     setIsModalOpen(false);
                 }}>Nie <i className="fa-solid fa-xmark"></i>
                 </button>
--- assets/ReactApp/tests/components/modal/content/drug-list/DeleteConfirmationModalContent.test.js
+++ assets/ReactApp/tests/components/modal/content/drug-list/DeleteConfirmationModalContent.test.js
@@ -6,19 +6,30 @@ import ReactShallowRenderer from "react-test-renderer/shallow";
 import DeleteConfirmationModalContent
     from "../../../../../components/modal/content/drug-list/DeleteConfirmationModalContent";
 import {fireEvent, render, screen} from "@testing-library/react";
+import {Provider} from "react-redux";
+import store from "../../../../../store";
+
+const setIsModalOpen = jest.fn();
+const confirmDeletion = jest.fn();
+
+function WrappedComponent() {
+    return (
+        <Provider store={store}>
+            <DeleteConfirmationModalContent setIsModalOpen={setIsModalOpen} drugId={1} confirmDeletion={confirmDeletion}/>
+        </Provider>
+    );
+}
 
 it('should correctly render DeleteConfirmationModalContent', () => {
     const renderer = new ReactShallowRenderer();
-    const setIsModalOpen = jest.fn();
 
-    renderer.render(<DeleteConfirmationModalContent setIsModalOpen={setIsModalOpen}/>);
+    renderer.render(<WrappedComponent/>);
 
     expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
 
 it('should close modal after clicking "No"', () => {
-    const setIsModalOpen = jest.fn();
-    render(<DeleteConfirmationModalContent setIsModalOpen={setIsModalOpen}/>);
+    render(<WrappedComponent/>);
 
     fireEvent.click(screen.getByTestId('no'));
 
