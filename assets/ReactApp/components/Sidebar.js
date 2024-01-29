5e803cb77db4331d0a27c2676538667b88f5fd0f
Michael Slabikovsky <michal.slabik.programista@gmail.com>
2024-01-29 17:45:36 +0100

    Bugfix: added closing sidebar in mobile view after choosing menu option

--- assets/ReactApp/components/Sidebar.js
+++ assets/ReactApp/components/Sidebar.js
@@ -3,25 +3,45 @@ import {NavLink} from "react-router-dom";
 import {mainRoute} from "../routers/AppRouter";
 
 function Sidebar() {
+    const closeMenu = () => document.querySelector('#sidebarMenu').classList.remove('show');
+
     return (
-        <nav id="sidebarMenu" className="collapse d-lg-block sidebar bg-white">
+        <nav id="sidebarMenu" className="collapse d-lg-block sidebar bg-white" role="navigation">
             <div className="position-sticky">
                 <div className="list-group list-group-flush mx-3 mt-4">
-                    <NavLink to={mainRoute} className="list-group-item list-group-item-action py-2 ripple" end data-bs-target="#sidebarMenu">
+                    <NavLink
+                        to={mainRoute}
+                        className="list-group-item list-group-item-action py-2 ripple"
+                        end
+                        data-testid="main-route-link"
+                        onClick={closeMenu}
+                    >
                         <i className="fas fa-tachometer-alt fa-fw me-3"></i><span>Strona główna</span>
                     </NavLink>
-                    <NavLink to={`${mainRoute}/drug-list`} className="list-group-item list-group-item-action py-2 ripple">
+                    <NavLink
+                        to={`${mainRoute}/drug-list`}
+                        className="list-group-item list-group-item-action py-2 ripple"
+                        onClick={closeMenu}
+                    >
                         <i className="fa-solid fa-jar fa-fw me-3"></i><span>Moje leki i suplementy</span>
                     </NavLink>
-                    <NavLink to={`${mainRoute}/profile`} className="list-group-item list-group-item-action py-2 ripple">
+                    <NavLink
+                        to={`${mainRoute}/profile`}
+                        className="list-group-item list-group-item-action py-2 ripple"
+                        onClick={closeMenu}
+                    >
                         <i className="fa-solid fa-user fa-fw me-3"></i><span>Mój profil</span>
                     </NavLink>
-                    <NavLink to={`${mainRoute}/settings`} className="list-group-item list-group-item-action py-2 ripple">
+                    <NavLink
+                        to={`${mainRoute}/settings`}
+                        className="list-group-item list-group-item-action py-2 ripple"
+                        onClick={closeMenu}
+                    >
                         <i className="fa-solid fa-gear fa-fw me-3"></i><span>Ustawienia</span>
                     </NavLink>
-                    <a href="/logout" className="list-group-item list-group-item-action py-2 ripple"
-                    ><i className="fa-solid fa-power-off fa-fw me-3"></i><span>Wyloguj się</span></a
-                    >
+                    <a href="/logout" className="list-group-item list-group-item-action py-2 ripple">
+                        <i className="fa-solid fa-power-off fa-fw me-3"></i><span>Wyloguj się</span>
+                    </a>
                 </div>
             </div>
         </nav>
--- assets/ReactApp/tests/components/Sidebar.test.js
+++ assets/ReactApp/tests/components/Sidebar.test.js
@@ -1,10 +1,27 @@
+/**
+ * @jest-environment jsdom
+ * */
 import React from "react";
 import ReactShallowRenderer from "react-test-renderer/shallow";
 import Sidebar from "../../components/Sidebar";
+import {fireEvent, render, screen} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
 
 it('should correctly render sidebar', () => {
     const renderer = new ReactShallowRenderer();
     renderer.render(<Sidebar/>);
 
     expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+it('should close sidebar menu after clicking a link', () => {
+    render(
+        <BrowserRouter>
+            <Sidebar/>
+        </BrowserRouter>
+    );
+
+    fireEvent.click(screen.getByTestId('main-route-link'));
+
+    expect(screen.getByRole('navigation')).not.toHaveClass('show');
 });
