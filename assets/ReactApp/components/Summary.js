97df16543445cbf0fc8bbf1cb4b41ba088f8a9b0
Michael Slabikovsky <michal.slabik.programista@gmail.com>
2024-03-26 14:12:07 +0100

    Removed unused import

--- assets/ReactApp/components/Summary.js
+++ assets/ReactApp/components/Summary.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import DrugList from "./DrugList";
 import StockStatusChecker from "./StockStatusChecker";
 import {useSelector} from "react-redux";
