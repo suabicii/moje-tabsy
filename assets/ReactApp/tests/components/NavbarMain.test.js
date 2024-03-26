9f57cb34a85273ee6344160f9079b95e53899536
Michael Slabikovsky <michal.slabik.programista@gmail.com>
2024-03-26 18:29:42 +0100

    Added theme change to NavbarMain

--- assets/ReactApp/tests/components/NavbarMain.test.js
+++ assets/ReactApp/tests/components/NavbarMain.test.js
@@ -1,10 +1,59 @@
+/**
+ * @jest-environment jsdom
+ * */
 import React from "react";
 import ReactShallowRenderer from "react-test-renderer/shallow";
 import NavbarMain from "../../components/NavbarMain";
+import {Provider} from "react-redux";
+import store from "../../store";
+import {fireEvent, render, screen} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
+import Sidebar from "../../components/Sidebar";
+
+function WrappedComponent() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <NavbarMain/>
+                <Sidebar/>
+            </BrowserRouter>
+        </Provider>
+    );
+}
+
+const changeTheme = require('../../utils/changeTheme');
+let changeThemeMock;
+
+
+beforeEach(() => {
+    changeThemeMock = jest.spyOn(changeTheme, 'default');
+});
+
+afterAll(() => {
+    jest.clearAllMocks();
+});
 
 it('should correctly render main navbar', () => {
     const renderer = new ReactShallowRenderer();
-    renderer.render(<NavbarMain/>);
+    renderer.render(
+        <Provider store={store}>
+            <NavbarMain/>
+        </Provider>
+    );
 
     expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+it('should change theme to light', () => {
+    render(<WrappedComponent/>);
+
+    expect(changeThemeMock).toHaveBeenLastCalledWith(false);
+});
+
+it('should change theme to dark after clicking toggler', () => {
+    render(<WrappedComponent/>);
+
+    fireEvent.click(screen.getByTestId('dark-theme-toggle'));
+
+    expect(changeThemeMock).toBeCalledWith(true);
 });
